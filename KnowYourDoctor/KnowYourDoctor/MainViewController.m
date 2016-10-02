3c8e5c85f5168fbd76077c11e778bfd54632259c
TUNG <tunghuynh@MACs-MacBook-Pro-3.local>
2016-10-02 12:25:44 +0700

    Fix request

--- KnowYourDoctor/KnowYourDoctor/MainViewController.m
+++ KnowYourDoctor/KnowYourDoctor/MainViewController.m
@@ -23,6 +23,8 @@
 }
 @property (weak, nonatomic) IBOutlet UIView *containerTab;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *segmentTab;
+@property (weak, nonatomic) IBOutlet UIToolbar *bottomBar;
+@property (weak, nonatomic) IBOutlet UIProgressView *processView;
 
 @end
 
@@ -34,6 +36,7 @@
     theConfiguration = [[WKWebViewConfiguration alloc] init];
     webView = [[WKWebView alloc] initWithFrame:CGRectMake(self.view.frame.origin.x, self.containerTab.frame.size.height, self.view.frame.size.width, self.view.frame.size.height) configuration:theConfiguration];
     webView.navigationDelegate = self;
+    [webView addObserver:self forKeyPath:NSStringFromSelector(@selector(estimatedProgress)) options:NSKeyValueObservingOptionNew context:NULL];
     
     [self setupView];
     [self loadPage:FirstPage];
@@ -44,12 +47,30 @@
     [self.navigationController.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor whiteColor]}];
 }
 
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([keyPath isEqualToString:NSStringFromSelector(@selector(estimatedProgress))] && object == webView) {
+        [self.processView setAlpha:1.0f];
+        [self.processView setProgress:webView.estimatedProgress animated:YES];
+        
+        if(webView.estimatedProgress >= 1.0f) {
+            [UIView animateWithDuration:0.3 delay:0.3 options:UIViewAnimationOptionCurveEaseOut animations:^{
+                [self.processView setAlpha:0.0f];
+            } completion:^(BOOL finished) {
+                [self.processView setProgress:0.0f animated:NO];
+            }];
+        }
+    }
+    else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
 - (void)loadPage: (NSString*)page {
     NSURL *nsurl = [NSURL URLWithString:page];
     
     NSURLRequest *nsrequest = [NSURLRequest requestWithURL:nsurl];
     [webView loadRequest:nsrequest];
-    [self.view addSubview:webView];
+    [self.view insertSubview:webView belowSubview:_bottomBar];
     spinView = [Help createSpinView:self.view];
     [spinView startAnimating];
 }
@@ -76,9 +97,34 @@
     }
 }
 
+- (IBAction)onTouchBack:(id)sender {
+    if ([webView canGoBack]) {
+        [webView goBack];
+    }
+}
+
+
+- (IBAction)onTouchNext:(id)sender {
+    if ([webView canGoForward]) {
+        [webView goForward];
+    }
+}
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc {
+    
+    if ([self isViewLoaded]) {
+        [webView removeObserver:self forKeyPath:NSStringFromSelector(@selector(estimatedProgress))];
+    }
+    
+    // if you have set either WKWebView delegate also set these to nil here
+    [webView setNavigationDelegate:nil];
+    [webView setUIDelegate:nil];
 }
 
 
